364f68bd1551ddad24e819b8cc91c4b179f472cc
Glitch (regal-safe-boar) <none>
2022-03-19 21:26:07 +0000

    model training and evaluating added

--- script.js
+++ script.js
@@ -86,3 +86,117 @@ function normalize(tensor, min, max) {
   return result;
 
 }
+
+
+
+
+
+// Normalize all input feature arrays and then 
+
+// dispose of the original non normalized Tensors.
+
+const FEATURE_RESULTS = normalize(INPUTS_TENSOR);
+
+console.log('Normalized Values:');
+
+FEATURE_RESULTS.NORMALIZED_VALUES.print();
+
+console.log('Min Values:');
+
+FEATURE_RESULTS.MIN_VALUES.print();
+
+console.log('Max Values:');
+
+FEATURE_RESULTS.MAX_VALUES.print();
+
+INPUTS_TENSOR.dispose();
+
+
+const model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [2], units: 1}));
+model.summary();
+
+train();
+
+
+
+async function train() {
+
+  const LEARNING_RATE = 0.01; // Choose a learning rate that’s suitable for the data we are using.
+
+  
+
+  // Compile the model with the defined learning rate and specify a loss function to use.
+
+  model.compile({
+
+    optimizer: tf.train.sgd(LEARNING_RATE),
+
+    loss: 'meanSquaredError'
+
+  });
+
+  // Finally do the training itself.
+
+  let results = await model.fit(FEATURE_RESULTS.NORMALIZED_VALUES, OUTPUTS_TENSOR, {
+
+    validationSplit: 0.15, // Take aside 15% of the data to use for validation testing.
+
+    shuffle: true,         // Ensure data is shuffled in case it was in an order
+
+    batchSize: 64,         // As we have a lot of training data, batch size is set to 64.
+
+    epochs: 10             // Go over the data 10 times!
+
+  });
+
+  
+
+  OUTPUTS_TENSOR.dispose();
+
+  FEATURE_RESULTS.NORMALIZED_VALUES.dispose();
+
+  console.log("Average error loss: " + Math.sqrt(results.history.loss[results.history.loss.length - 1]));
+
+  console.log("Average validation error loss: " +   
+
+      Math.sqrt(results.history.val_loss[results.history.val_loss.length - 1]));
+
+    
+
+  evaluate(); // Once trained evaluate the model.
+
+}
+
+
+function evaluate() {
+
+  // Predict answer for a single piece of data.
+
+  tf.tidy(function() {
+
+    let newInput = normalize(tf.tensor2d([[750, 1]]), FEATURE_RESULTS.MIN_VALUES, FEATURE_RESULTS.MAX_VALUES);
+
+    let output = model.predict(newInput.NORMALIZED_VALUES);
+
+    output.print();
+
+  });
+
+  
+
+  // Finally when you no longer need to make any more predictions,
+
+  // clean up the remaining Tensors. 
+
+  FEATURE_RESULTS.MIN_VALUES.dispose();
+
+  FEATURE_RESULTS.MAX_VALUES.dispose();
+
+  model.dispose();
+
+  
+
+  console.log(tf.memory().numTensors);
+
+}
